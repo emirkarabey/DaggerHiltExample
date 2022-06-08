04593734978813e6457c3b8aa634b979ebd2441a
emirk <emir.karabey1@hotmail.com>
2022-06-08 16:18:07 +0300

    Last commit

--- app/src/main/java/com/example/daggerhiltexample/MainViewModel.kt
+++ app/src/main/java/com/example/daggerhiltexample/MainViewModel.kt
@@ -2,8 +2,9 @@ package com.example.daggerhiltexample
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
-
+@HiltViewModel
 class MainViewModel @Inject constructor(private val repository:RetrofitRepository):ViewModel() {
 
     var liveData:MutableLiveData<List<Post>>
